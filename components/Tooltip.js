0a33263120129085f6ed4324e450a8e47a6fe970
CodingExplored <144565131+CodingExplored@users.noreply.github.com>
2023-09-26 08:50:26 -0500

    Update Tooltip.js

--- components/Tooltip.js
+++ components/Tooltip.js
@@ -5,10 +5,10 @@ const Tooltip = ({ text, children, style }) => {
   return (
     // The outer wrapper for the tooltip component. Any additional styles passed via 'style' prop are applied here.
     <div style={style} className="tooltip">
-      
+
       {/* The tooltip text to be displayed */}
       <span className="tooltip-text">{text}</span>
-      
+
       {/* The content over which the tooltip will appear */}
       {children}
     </div>
